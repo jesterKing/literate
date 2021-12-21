d05f2430dd6fa5f71c3cdaaa55feb317f910ab98
Nathan Letwory <nathan@mcneel.com>
2021-12-21 11:06:09 +0200

    Ensure working with vscode 1.63.2

--- src/extension.ts
+++ src/extension.ts
@@ -60,7 +60,6 @@ export function activate(context: vscode.ExtensionContext) {
 	const diagnostics = vscode.languages.createDiagnosticCollection('literate');
 	//setupLanguageMapping();
 
-	
 		// The command has been defined in the package.json file
 		// Now provide the implementation of the command with registerCommand
 		// The commandId parameter must match the command field in package.json
