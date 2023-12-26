b73a149c944882b28129679ce80af8a48adc9ec7
Nathan Letwory <nathan@letworyinteractive.com>
2023-12-26 11:33:07 +0200

    Remove unused import.

--- src/extension.ts
+++ src/extension.ts
@@ -13,7 +13,6 @@
 import { TextDecoder } from 'util';
 import * as vscode from 'vscode';
 import * as path from 'path';
-import * as fs from 'fs';
 import * as os from 'os';
 
 import StateCore = require('markdown-it/lib/rules_core/state_core');
